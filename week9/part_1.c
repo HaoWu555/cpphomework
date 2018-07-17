eb7726611336193fb2834417e3d77c093d0fe5c6
Hao Wu <hao.wu@terranet.se>
2018-07-17 21:54:29 +0200

    modify week9 homework

--- week9/part_1.c
+++ week9/part_1.c
@@ -1,11 +1,41 @@
-#include <iostream>
-#include <iterator>
-#include <set>
+#include<vector>
+#include<iostream>
+#include<algorithm>
 using namespace std;
-int main() {
-    int a[] = {8,7,8,9,6,2,1};
-// 在此处补充你的代码
-    ostream_iterator<int> o(cout," ");
-    copy( v.begin(),v.end(),o);
-    return 0;
+
+struct Point{
+	int x;
+	int y;
+	Point(int x_, int y_):x(x_),y(y_){}
+
+};
+
+bool operator<(const Point & p1, const Point & p2){
+	if (p1.y<p2.y)
+		return true;
+	else if (p1.y==p2.y)
+		return p1.x < p2.x;
+	else
+		return false;
+}
+
+int main(){
+	int t;
+ 	int x,y;
+	cin >> t;
+	vector<Point> v;
+	while(t--){
+		cin>>x>>y;
+		v.push_back(Point(x,y));
+	}
+	vector<Point >::iterator i,j;
+	int nTotalNum =0;
+
+	sort(v.begin(),v.end());
+	
+	for (i=v.begin(); i!=v.end();i++){
+		cout<<i->x <<" "<< i-> y << endl;
+	}
+
+return 0;
 }
